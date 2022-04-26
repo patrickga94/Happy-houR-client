35c0ddb5d9f49f34e7bc7b9ef076812a65cbec65
patrickga <patrick.g.alvarez@gmail.com>
2022-04-26 08:42:24 -0500

    Adds tags to the happy hour index page

--- src/components/happy-hours/IndexHappyHours.js
+++ src/components/happy-hours/IndexHappyHours.js
@@ -27,20 +27,17 @@ const IndexHappyHours = (props) => {
         return <p>No happy hours here, this town is a snooze fest!</p>
     }
 
+    
+    let tagPills
 
     let happyHourCards
     if(happyHours.length > 0){
         happyHourCards = happyHours.map(happyHour => {
-            // let tagPills
-            // if (happyHour.tags.length > 0){
-            //     tagPills = happyHour.tags.map(tag => (
-            //         <ShowTag 
-            //             tag={tag} 
-            //             user={user} 
-            //             happyHour={happyHour}
-            //         />
-            //     ))
-            // }
+                if (happyHour.tags.length > 0){
+                    tagPills = happyHour.tags.map(tag => (
+                        <span key={tag._id}>{tag.tag.toUpperCase()} </span>
+                    ))
+                }
             return (
                 <Card key={happyHour._id} style={{width: '30%' }} className="m-2 shadow p-3 mb-5 bg-body rounded">
                 <Card.Header>{happyHour.name} at {happyHour.owner.username}</Card.Header>
@@ -51,7 +48,7 @@ const IndexHappyHours = (props) => {
                     </Card.Text>
                 </Card.Body>
                 <Card.Footer>
-                    {/* Tags: {happyHour.tags.length > 0 ? {tagPills}: <span>None</span>}   */}
+                    Tags: {happyHour.tags.length > 0 ? <div>{tagPills}</div>: <span>None</span>}  
                 </Card.Footer>
             </Card> 
             )
